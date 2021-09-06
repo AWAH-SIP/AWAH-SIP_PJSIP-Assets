57429dfc485c07622dadadafd0fc522acf858d51
Adrian Hilber <adi.hilber@gmail.com>
2021-09-06 23:09:25 +0200

    Update entrypoint.sh

--- linux_arm_rpi4/Raspberry-Build-Docker/entrypoint.sh
+++ linux_arm_rpi4/Raspberry-Build-Docker/entrypoint.sh
@@ -9,15 +9,16 @@ if [ "$1" = "build" ]; then
 		echo "Mount point ${APP_SRCDIR} is empty, did you invoke the container with \"docker run --rm -v /path/to/pjsip/project:/var/build build\"?"
 		exit 0
 	else
+        sudo su
 		mkdir -p $APP_BUILDDIR
 		cd $APP_SRCDIR
         export PATH=$PATH:/src/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf/bin
         export CPPFLAGS="-I/src/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf/lib/gcc/arm-linux-gnueabihf/7.4.1/include -I/src/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf/include -I/src/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf/arm-linux-gnueabihf/libc/usr/include -I/src/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf/arm-linux-gnueabihf/include -I/sysroot/opt/vc/include -I/sysroot/usr/lib/gcc/arm-linux-gnueabihf/8/include -I/sysroot/usr/include"
         printenv
         ./configure --host=arm-linux-gnueabihf CROSS_COMPILE="/src/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-" CC="/src/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc" LD="/src/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc"  CFLAGS="--sysroot=/sysroot" LDFLAGS="-L/src/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf/arm-linux-gnueabihf/libc/lib -L/sysroot/lib/arm-linux-gnueabihf/ -ldl -lc  --sysroot=/sysroot" CPPFLAGS="" --prefix=$APP_BUILDDIR 
-		sudo make dep
-        sudo make -j4
-        sudo make install
+		make dep
+        make -j4
+        make install
 	fi
 else
 	cd ~
